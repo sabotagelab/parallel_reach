55018e1845eb7730e57685113ca941f34dbaa7a9
niraj <nirajbasnet2008@gmail.com>
2020-11-10 19:45:00 -0800

    fixing viz timing, param loading for zono node

--- reachability/scripts/hylaa_viz_node.py
+++ reachability/scripts/hylaa_viz_node.py
@@ -9,7 +9,7 @@ from osuf1_common.msg import ReachSets
 import tripy
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Pose, Point, Quaternion, Vector3
-from std_msgs.msg import Header, ColorRGBA
+from std_msgs.msg import Header, ColorRGBA, Duration
 from osuf1_common.msg import ReachSets
 from itertools import repeat
 
@@ -25,7 +25,7 @@ class Hylaa_Viz_Node:
         sets_outline_topic = rospy.get_param("/hylaa_viz/outline_topic", "hylaa_viz_outline")
         sets_tris_topic = rospy.get_param("/hylaa_viz/tris_topic", "hylaa_viz_tris")
 
-        self.outlinePub = rospy.Publisher(sets_outline_topic, MarkerArray, queue_size=1)
+        self.outlinePub = rospy.Publisher(sets_outline_topic, MarkerArray, queue_size=100)
         self.trisPub = rospy.Publisher(sets_tris_topic, Marker, queue_size=1)
 
         self.tri_marker_id = 44
@@ -34,12 +34,18 @@ class Hylaa_Viz_Node:
         self.colors = [(230, 25, 75), (60, 180, 75), (255, 225, 25), (0, 130, 200), (245, 130, 48), (145, 30, 180), (70, 240, 240), (240, 50, 230), (210, 245, 60), (250, 190, 190), (0, 128, 128), (230, 190, 255), (170, 110, 40), (255, 250, 200), (128, 0, 0), (170, 255, 195), (128, 128, 0), (255, 215, 180), (0, 0, 128), (128, 128, 128)]
         self.colors = [ColorRGBA(c[0], c[1], c[2], 1) for c in self.colors]
 
+        self.skipfreq=2
+        self.skipcount=0
+
     def start(self):
         rospy.spin()
     
     def makeViz(self, reachSets):
+#        if self.skipfreq == self.skipcount:
+#            self.skipcount = 0
+#        self.skipcount += 1
         pointSets = [[tuple(p.p) for p in rs.set] for rs in reachSets.sets]
-        triangleSets = [tripy.earclip(ps) for ps in pointSets]
+        #triangleSets = [tripy.earclip(ps) for ps in pointSets]
 
 
         header = Header()
@@ -57,6 +63,8 @@ class Hylaa_Viz_Node:
             m.action = 0
             m.pose = origin
             m.type = 4 #LINE_STRIP
+
+
             m.color = self.colors[ii%len(self.colors)]
             m.points = [Point(p[0], p[1], 0) for p in pointSets[ii]]
             m.scale = Vector3(.05, 0, 0)
@@ -65,6 +73,7 @@ class Hylaa_Viz_Node:
         lineMarkers.markers = lineMarkerArray 
 
         self.outlinePub.publish(lineMarkers)
+        rospy.loginfo("Published Visualization")
 
 
         #triPoints = [ xy for tri in triangleSets for xy in tri]
--- reachability/scripts/zono_node.py
+++ reachability/scripts/zono_node.py
@@ -24,29 +24,30 @@ class ZONO_Node:
         #                   REACHABILITY Params
         #---------------------------------------------------------
         self.predictions = None #predictions from MPC
-        self.reachability_horizon = rospy.get_param("/zono_node/horizon", 0)
-        self.runtime_mode = rospy.get_param("/zono_node/runtime_mode", "CPU")
+        self.reachability_horizon = rospy.get_param("zono_node/horizon", 0)
+        self.runtime_mode = rospy.get_param("zono_node/runtime_mode", "CPU")
         self.dt = None          #dt from MPC metdata
         self.mpc_horizon = None #simulation horizon from MPC metadata
 
         #TODO automatic or otherwise validated variability
         self.state_uncertainty = [
-            rospy.get_param("/zono_node/state_uncertainty/px", 0),
-            rospy.get_param("/zono_node/state_uncertainty/py", 0),
-            rospy.get_param("/zono_node/state_uncertainty/psi", 0),
+            rospy.get_param("zono_node/state_uncertainty/px", 0),
+            rospy.get_param("zono_node/state_uncertainty/py", 0),
+            rospy.get_param("zono_node/state_uncertainty/psi", 0),
         ]
+        rospy.loginfo(self.state_uncertainty)
 
         self.input_uncertainty = [
-            rospy.get_param("/zono_node/input_uncertainty/velocity", 0),
-            rospy.get_param("/zono_node/input_uncertainty/delta", 0)
+            rospy.get_param("zono_node/input_uncertainty/velocity", 0),
+            rospy.get_param("zono_node/input_uncertainty/delta", 0)
         ]
 
 
         #custom horizon or utilize all computed MPC steps
-        self.hylaa_verbosity = rospy.get_param("/zono_node/output_verbosity", "VERBOSE").upper() #TODO integrate with roslogger 
-        self.graph_predictions = rospy.get_param("/zono_node/graph_predictions", False) #wether predicted sim results should be overlayed on reach set graph (if displayed)
-        self.displayType = rospy.get_param("/zono_node/display_type", "NONE").upper()
-        self.output = rospy.get_param("/zono_node/display_filename", "hylaa_reach.png") #TODO put images in distinct folder and label with frame times
+        self.hylaa_verbosity = rospy.get_param("zono_node/output_verbosity", "VERBOSE").upper() #TODO integrate with roslogger 
+        self.graph_predictions = rospy.get_param("zono_node/graph_predictions", False) #wether predicted sim results should be overlayed on reach set graph (if displayed)
+        self.displayType = rospy.get_param("zono_node/display_type", "NONE").upper()
+        self.output = rospy.get_param("zono_node/display_filename", "hylaa_reach.png") #TODO put images in distinct folder and label with frame times
 
 
         #---------------------------------------------------------
@@ -56,7 +57,7 @@ class ZONO_Node:
         self.QuickZono.set_model_params(self.state_uncertainty, self.input_uncertainty, "model_hardcode", runtime_mode=self.runtime_mode)
 
         #custom interval in ms or 0=maximum speed
-        self.reachability_interval = rospy.get_param("/zono_node/interval", 0)
+        self.reachability_interval = rospy.get_param("zono_node/interval", 0)
 
         self.current_metadata = False
         self.current_trajectory = False
@@ -65,9 +66,9 @@ class ZONO_Node:
         #---------------------------------------------------------
         #                   NODE Params
         #---------------------------------------------------------
-        self.prediction_topic = rospy.get_param("/zono_node/mpc_prediction_topic", "mpc_prediction") #topic which publishes predicted trajectory
-        self.metadata_topic = rospy.get_param("/zono_node/mpc_metadata_topic", "mpc_metadata")
-        self.reach_pub_topic = rospy.get_param("/zono_node/reach_pub_topic", "hylaa_reach") #topic where reach sets are published
+        self.prediction_topic = rospy.get_param("zono_node/mpc_prediction_topic", "mpc_prediction") #topic which publishes predicted trajectory
+        self.metadata_topic = rospy.get_param("zono_node/mpc_metadata_topic", "mpc_metadata")
+        self.reach_pub_topic = rospy.get_param("zono_node/reach_pub_topic", "hylaa_reach") #topic where reach sets are published
 
         self.prediction_sub = rospy.Subscriber(self.prediction_topic, MPC_trajectory, self.storePredictions)
         self.metadata_sub = rospy.Subscriber(self.metadata_topic, MPC_metadata, self.storeMetadata)
@@ -92,6 +93,8 @@ class ZONO_Node:
                 end = time.perf_counter()
                 rospy.loginfo("QuickZono reachability computation finished.") #TODO add time to output
                 rospy.loginfo("ITER TIME WAS {}".format(end-start))
+                rospy.loginfo(f"Steps is {len(reach)}, dt is {self.dt}, horizon is {self.reachability_horizon}")
+                rospy.loginfo(f"Mode is {self.runtime_mode}")
 
                 #build message to publish reachset
                 reachMessage = ReachSets()
